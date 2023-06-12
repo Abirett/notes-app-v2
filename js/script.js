06609366918898c47e33c34d0a3da2e6d2fa3b3b
Ashfaque Hossain Abir <104485722+Abirett@users.noreply.github.com>
2023-06-12 21:44:56 +0545

    Files Added

---
+++ js/script.js
@@ -0,0 +1,169 @@
+/*=========================================================
+-- copyright 2023 A.H. Abir (Notes App Updated Version)
+=========================================================*/
+
+const addBox = document.querySelector(".add-box"),
+popupBox = document.querySelector(".popup-box"),
+popupTitle = popupBox.querySelector("header p"),
+closeIcon = popupBox.querySelector("header i"),
+titleTag = popupBox.querySelector("input"),
+descTag = popupBox.querySelector("textarea"),
+addBtn = popupBox.querySelector("button");
+
+const months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let isUpdate = false, updateId;
+
+addBox.addEventListener("click", () => {
+    popupTitle.innerText = "Add a new Note";
+    addBtn.innerText = "Add Note";
+    popupBox.classList.add("show");
+    document.querySelector("body").style.overflow = "hidden";
+    if(window.innerWidth > 660) titleTag.focus();
+});
+
+closeIcon.addEventListener("click", () => {
+    isUpdate = false;
+    titleTag.value = descTag.value = "";
+    popupBox.classList.remove("show");
+    document.querySelector("body").style.overflow = "auto";
+});
+
+function showNotes() {
+    if(!notes) return;
+    document.querySelectorAll(".note").forEach(li => li.remove());
+    notes.forEach((note, id) => {
+        let filterDesc = note.description.replaceAll("\n", '<br/>');
+        let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${filterDesc}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="menu">
+                                    <li onclick="updateNote(${id}, '${note.title}', '${filterDesc}')"><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick="deleteNote(${id})"><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+showNotes();
+
+function showMenu(elem) {
+    elem.parentElement.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != elem) {
+            elem.parentElement.classList.remove("show");
+        }
+    });
+}
+
+function deleteNote(noteId) {
+    let confirmDel = confirm("Are you sure you want to delete this note?");
+    if(!confirmDel) return;
+    notes.splice(noteId, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+}
+
+function updateNote(noteId, title, filterDesc) {
+    let description = filterDesc.replaceAll('<br/>', '\r\n');
+    updateId = noteId;
+    isUpdate = true;
+    addBox.click();
+    titleTag.value = title;
+    descTag.value = description;
+    popupTitle.innerText = "Update a Note";
+    addBtn.innerText = "Update Note";
+}
+
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    let title = titleTag.value.trim(),
+    description = descTag.value.trim();
+
+    if(title || description) {
+        let currentDate = new Date(),
+        month = months[currentDate.getMonth()],
+        day = currentDate.getDate(),
+        year = currentDate.getFullYear();
+
+        let noteInfo = {title, description, date: `${month} ${day}, ${year}`}
+        if(!isUpdate) {
+            notes.push(noteInfo);
+        } else {
+            isUpdate = false;
+            notes[updateId] = noteInfo;
+        }
+        localStorage.setItem("notes", JSON.stringify(notes));
+        showNotes();
+        closeIcon.click();
+    }
+});
+
+
+//-------------- For prevent hacking -----------------
+
+// Disable context menu
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+   }, false);
+  
+   // Disable “Ctrl+Shift+I”, “Ctrl+U” and ”F12 key
+   document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey || e.keyCode==123) {
+     e.stopPropagation();
+     e.preventDefault();
+    }
+   });
+
+
+//========================================================================
+//-- Username Popup Box (Additional Feature in this update)
+//========================================================================
+
+// Creating functions for Popup Box
+   window.addEventListener("load", function() {
+    setTimeout(
+        function open(event){
+            document.getElementById("overlay").style.display = "block";
+            document.querySelector(".user-popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+document.querySelector("#close").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+    document.querySelector(".user-popup").style.display = "none";
+});
+
+// After Submission User popup will close
+const submitButton = document.querySelector('.submitBtn');
+const userPopup = document.querySelector('.user-popup');
+const overlay = document.getElementById('overlay');
+
+submitButton.addEventListener('click', () => {
+    userPopup.style.display = 'none';
+    overlay.style.display = 'none';
+});
+
+// Transfer Input value to inner HTML
+function submit() {
+    var inputValue = document.getElementById("input").value;
+    // document.getElementById("username").innerHTML = inputValue + "!";
+
+    // Applying conditions for submit value
+    if (inputValue == "" || inputValue == null) {
+        document.getElementById('username').innerHTML = "User!"
+    } else {
+        document.getElementById("username").innerHTML = inputValue + "!";
+    }
+  }
